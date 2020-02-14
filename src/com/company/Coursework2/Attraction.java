9be8606aa18103e2b0225026f5c417543f9c0d02
Abena <ecn19seu@uea.ac.uk>
2020-02-14 15:19:29 +0000

    Added files

---
+++ src/com/company/Coursework2/Attraction.java
@@ -0,0 +1,22 @@
+package com.company.Coursework2;
+/******************************************************************************
+
+ File        : Attraction.java
+
+ Date        : 12/02/2020
+
+ Author      : Abena Serwaa Johene
+
+ Description : Abstract Attraction Class. Base class for the Transportattraction, GentleAttraction and RollercoasterAttraction
+
+ History     : v 0.01
+
+ Copyright   : (c) Abena Serwaa Johene Amo
+
+ ******************************************************************************/
+
+public  abstract class Attraction {
+
+    public abstract int getOffPeakPrice();
+
+}
---
+++ src/com/company/Coursework2/GentleAttraction.java
@@ -0,0 +1,48 @@
+package com.company.Coursework2;
+/******************************************************************************
+
+ File        : TransportAttraction.java
+
+ Date        : 12/02/2020
+
+ Author      : Abena Serwaa Johene.
+
+ Description : Sub/Child class of Attraction. This class is to store information and perform methods for
+ gentle attractions which includes:  Ferris wheels and mini golf.
+
+ History     : v 0.01
+
+ Copyright   : (c) Abena Serwaa Johene Amo.
+
+ ******************************************************************************/
+
+public class GentleAttraction extends Attraction {
+    String name;
+    //Accessor method for name
+    public String getName() {
+        return name;
+    }
+    //Mutator method to alter the name of an Attraction
+    public void setName(String name) {
+        this.name = name;
+    }
+    int basePrice;
+    //Accessor method for base price
+    public int getBasePrice() {
+        return basePrice;
+    }
+    //Mutator method to alter the base price.
+    public void setBasePrice(int basePrice) {
+        this.basePrice = basePrice;
+    }
+    //Constructor to create transport attraction objects
+    public GentleAttraction(String name, int basePrice){
+        this.name=name;
+        this.basePrice=basePrice;
+    }
+
+    @Override
+    public int getOffPeakPrice() {
+        return 0;
+    }
+}
